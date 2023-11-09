a9b8f3f19907d9b36325b45dff5614542b65152f
rayssabuarque <brayssa17@gmail.com>
2023-11-09 19:52:15 -0300

    implementando visibilidade

--- src/app/pages/pagamento/pagamento.component.ts
+++ src/app/pages/pagamento/pagamento.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagamentoComponent implements OnInit {
 
+
+  //criando uma variável pra colocar a classe de visibilidade
+  view_Endereco:string = 'aparecendo'
+  view_Entrega:string = 'escondido'
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  //FUNÇÃO QUE MUDA A CLASSE DE VISIBILIDADE
+  mudar_viewEntrega(){
+    //mostra a entrega
+    this.view_Entrega = 'aparecendo'
+
+    //esconde o endereco
+    this.view_Endereco = 'escondido'
   }
 
 }
