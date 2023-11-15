69c2bc946e0caf02bba8dfc23cfe7e7388735a3a
rayssabuarque <brayssa17@gmail.com>
2023-11-14 22:31:01 -0300

    sebos da lista

--- src/app/pages/sebos-lista/sebos-lista.component.ts
+++ src/app/pages/sebos-lista/sebos-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from 'src/app/services/crud/crud.service';
 
 @Component({
   selector: 'app-sebos-lista',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SebosListaComponent implements OnInit {
 
-  constructor() { }
+  sebosIds:string[] = []
+
+  constructor( private crud:CrudService) { }
 
   ngOnInit(): void {
+    this.getSebos()
+  }
+
+  // Guardando IDs de sebos
+  getSebos():void{
+
+    this.crud.read('/users','','?get_Sebos=sim')
+      .then((res:any) => {
+        for(let i in res){
+          this.sebosIds.push(res[i].Id_usuario)
+        }
+      })
+
   }
 
 }
