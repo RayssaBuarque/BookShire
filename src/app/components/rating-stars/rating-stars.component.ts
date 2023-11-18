72b883813008952d1e511a27ee71f92843723149
rayssabuarque <brayssa17@gmail.com>
2023-11-18 13:57:06 -0300

    popup de avaliacao criado

---
+++ src/app/components/rating-stars/rating-stars.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-stars',
+  templateUrl: './rating-stars.component.html',
+  styleUrls: ['./rating-stars.component.css']
+})
+export class RatingStarsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
