b588ea9bca1fdef03e536cc619df3875c5fe1df0
Penninck05 <mcpr.penninck@gmail.com>
2023-11-23 11:58:38 -0300

    responsividade top_bar e nav_bar 23/11

--- src/app/components/top-nav-bar/top-nav-bar.component.ts
+++ src/app/components/top-nav-bar/top-nav-bar.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-nav-bar',
   templateUrl: './top-nav-bar.component.html',
-  styleUrls: ['./top-nav-bar.component.css']
+  styleUrls: ['./top-nav-bar.component.css','top-nav-bar.responsividade.css']
 })
 export class TopNavBarComponent implements OnInit {
+
+  top_bar_pc:string = 'escondido'
+  top_bar:string = 'aparecendo'
 
   constructor() { }
 
