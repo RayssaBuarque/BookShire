67d50e5cb592b84601d682a1f5135163f69dcf97
rayssabuarque <brayssa17@gmail.com>
2023-11-23 11:55:00 -0300

    atualizando os chats

--- src/app/pages/chat-geral/chat-geral.component.ts
+++ src/app/pages/chat-geral/chat-geral.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from 'src/app/services/crud/crud.service';
 
+import { userData } from 'src/assets/data/user_data';
+
 @Component({
   selector: 'app-chat-geral',
   templateUrl: './chat-geral.component.html',
@@ -9,7 +11,7 @@ import { CrudService } from 'src/app/services/crud/crud.service';
 export class ChatGeralComponent implements OnInit {
   
   //configurar cadastro mais tarde
-  idUsuario:string = '2'
+  idUsuario:string = userData.userId
 
   idChats:string[] = []
 
@@ -24,9 +26,38 @@ export class ChatGeralComponent implements OnInit {
   getConversas(){
     this.crud.read('/chat', '', '')
       .then((res:any) => {
-        for(let chats of res){
-          this.idChats.push(chats.Id_chat)
-        }
+        let idPedidos: any[] = []
+
+        //coletando chats onde User é cliente
+        this.crud.read('/pedidos', '', `?Id_cliente=${this.idUsuario}`)
+          .then((pCliente:any)=>{
+            for(let pedido of pCliente){
+              if(pedido.dataConclusao == null){
+                idPedidos.push(pedido.Id_pedido)
+              }
+            }
+            // console.log(res)
+
+            // coletando chats onde User é anunciante
+            this.crud.read('/pedidos', '', `?Id_anunciante=${this.idUsuario}`)
+              .then((pAnunciante:any) =>{
+                console.log(pAnunciante)
+                for(let pedido of pAnunciante){
+                  if(pedido.dataConclusao == null){
+                    idPedidos.push(pedido.Id_pedido)
+                  }
+                }
+
+                for(let chats of res){
+                  if(idPedidos.includes(chats.Id_pedido)){
+                    this.idChats.push(chats.Id_chat)
+                  }
+                }
+                // console.log(idPedidos)
+              })
+          })
+        
+        
       })
   }
 
